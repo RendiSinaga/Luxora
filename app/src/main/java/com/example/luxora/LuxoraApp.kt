db03db3aee0ba93ddf1c7fb58d5c8d36254e0745
RendiSinaga <rendysinagaa10@gmail.com>
2024-11-12 14:15:53 +0700

    first Commit

---
+++ app/src/main/java/com/example/luxora/LuxoraApp.kt
@@ -0,0 +1,13 @@
+package com.example.luxora
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Luxora(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()) {
+
+}
